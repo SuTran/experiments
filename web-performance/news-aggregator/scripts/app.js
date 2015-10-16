f307ff2f3182992cb3340d470773a62e0437353d
Su Tran <trannguyentiensu@gmail.com>
2015-10-16 23:28:06 +0700

    Performance improved: fix force synchronous layout

--- web-performance/news-aggregator/scripts/app.js
+++ web-performance/news-aggregator/scripts/app.js
@@ -256,6 +256,14 @@ APP.Main = (function() {
 
     var storyElements = document.querySelectorAll('.story');
 
+    // Base the scale on the y position of the score.
+    var height = main.offsetHeight;
+    var mainPosition = main.getBoundingClientRect();
+    var bodyTop = document.body.getBoundingClientRect().top;
+    var firstScore = storyElements[0].querySelector('.story__score');
+    var firstStoryTop = firstScore.getBoundingClientRect().top;
+    var firstStoryWidth = firstScore.getBoundingClientRect().width;
+    var storyOffsetWidth = storyElements[0].offsetWidth;
     // It does seem awfully broad to change all the
     // colors every time!
     for (var s = 0; s < storyElements.length; s++) {
@@ -264,11 +272,8 @@ APP.Main = (function() {
       var score = story.querySelector('.story__score');
       var title = story.querySelector('.story__title');
 
-      // Base the scale on the y position of the score.
-      var height = main.offsetHeight;
-      var mainPosition = main.getBoundingClientRect();
-      var scoreLocation = score.getBoundingClientRect().top -
-          document.body.getBoundingClientRect().top;
+      var scoreLocation = firstStoryTop * (s * storyOffsetWidth) -
+          bodyTop;
       var scale = Math.min(1, 1 - (0.05 * ((scoreLocation - 170) / height)));
       var opacity = Math.min(1, 1 - (0.5 * ((scoreLocation - 170) / height)));
 
@@ -277,8 +282,7 @@ APP.Main = (function() {
       score.style.lineHeight = (scale * 40) + 'px';
 
       // Now figure out how wide it is and use that to saturate it.
-      scoreLocation = score.getBoundingClientRect();
-      var saturation = (100 * ((scoreLocation.width - 38) / 2));
+      var saturation = (100 * ((firstStoryWidth - 38) / 2));
 
       score.style.backgroundColor = 'hsl(42, ' + saturation + '%, 50%)';
       title.style.opacity = opacity;
@@ -297,9 +301,12 @@ APP.Main = (function() {
 
   main.addEventListener('scroll', function() {
 
+    var scrollTop = main.scrollTop;
+    var scrollHeight = main.scrollHeight;
+    var offsetHeight = main.offsetHeight;
     var header = $('header');
     var headerTitles = header.querySelector('.header__title-wrapper');
-    var scrollTopCapped = Math.min(70, main.scrollTop);
+    var scrollTopCapped = Math.min(70, scrollTop);
     var scaleString = 'scale(' + (1 - (scrollTopCapped / 300)) + ')';
 
     colorizeAndScaleStories();
@@ -309,15 +316,15 @@ APP.Main = (function() {
     headerTitles.style.transform = scaleString;
 
     // Add a shadow to the header.
-    if (main.scrollTop > 70)
+    if (scrollTop > 70)
       document.body.classList.add('raised');
     else
       document.body.classList.remove('raised');
 
     // Check if we need to load the next batch of stories.
-    var loadThreshold = (main.scrollHeight - main.offsetHeight -
+    var loadThreshold = (scrollHeight - offsetHeight -
         LAZY_LOAD_THRESHOLD);
-    if (main.scrollTop > loadThreshold)
+    if (scrollTop > loadThreshold)
       loadStoryBatch();
   });
 
